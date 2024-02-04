b6dbaa2efcffc735f5221d9e66f9c62634fd00cc
yusufmafif <yusufmafif@gmail.com>
2024-02-04 23:14:05 +0700

    .

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
+app.length("/", (req, res) => {
+    res.send("Hello World");
+})
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
