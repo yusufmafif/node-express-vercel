33ef0f6a7d66de6b32e77611e1585cc2858c5cd4
yusufmafif <yusufmafif@gmail.com>
2024-02-04 23:35:43 +0700

    s

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const home = require("./routes/home");
 
 // Middlewares
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
 // Routes
-app.use("/home", home);
+// app.use("/home", home);
+app.get("/", (req, res) => {
+    res.send("Express Testing");
+});
 
 // connection
 const port = process.env.PORT || 9001;
