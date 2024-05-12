7a3f453845abf09128ebd69c76a4ed8c480ed14e
yusufmafif <yusufmafif@gmail.com>
2024-05-13 00:03:48 +0700

    cors non

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const cors = require("cors");
 // Middlewares
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 // Routes
 app.use("/", home);
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 
 const productController = require("./product/product.controller");
 const productsController = require("./product/products.controller");
