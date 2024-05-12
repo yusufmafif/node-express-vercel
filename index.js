8e49208567d44cea21c673aa029d3aa01bdf0f6a
yusufmafif <yusufmafif@gmail.com>
2024-05-13 01:36:00 +0700

    cors check 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Use cors middleware to allow requests from any origin
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://belajarexpress.vercel.app");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", " Content-Type, Authorization");
     next();
