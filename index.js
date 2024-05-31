6fb6aac4dc7085b60542b0cd1ec2e8960cfa6ace
yusufmafif <yusufmafif@gmail.com>
2024-05-31 20:55:22 +0700

    ...

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", process.env.DEV_ADDRESS);
+    res.setHeader("Access-Control-Allow-Origin", "https://cashier-pos.netlify.app");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
