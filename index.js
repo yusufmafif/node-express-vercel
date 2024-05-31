95a82a88d503566fe80b441ed8f9a691abf84f0b
yusufmafif <yusufmafif@gmail.com>
2024-05-31 20:34:30 +0700

    a

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+    res.setHeader("Access-Control-Allow-Origin", process.env.DEV_ADDRESS);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
 next();
 });
 
