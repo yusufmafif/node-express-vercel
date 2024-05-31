42d39aa522e46faa0eb524c4c2886aedb2551328
yusufmafif <yusufmafif@gmail.com>
2024-05-31 18:49:41 +0700

    test

--- index.js
+++ index.js
@@ -2,27 +2,16 @@
 const express = require("express");
 const home = require("./routes/home");
 const app = express();
-const cors = require('cors')
-
-const corsOptions = {
-    origin: 'https://cashier-pos.netlify.app',
-    credentials: true,
-
-}
-app.use(cors(corsOptions));
-
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", process.env.DEV_ADDRESS);
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-// next();
-// });
-
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Routes
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", process.env.DEV_ADDRESS);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+next();
+});
+
 app.use("/", home);
 
 const productController = require("./product/product.controller");
