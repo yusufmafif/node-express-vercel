325cebd3bdc2a01a90094edade05ffd3795ca87b
yusufmafif <yusufmafif@gmail.com>
2024-05-31 19:27:37 +0700

    { httpOnly: false}

--- product/login.controller.js
+++ product/login.controller.js
@@ -54,7 +54,7 @@ router.post("/", async (req, res) => {
         //     id : payload.id
         // })
 
-        return res.cookie("token", token, { httpOnly: false, sameSite : "none", secure: false }).json({
+        return res.cookie("token", token, { httpOnly: false}).json({
             data: {
                 id: payload.id,
                 username: payload.username,
