3c9fcdf0c62302c32bbd5bcce1228853c62f8d44
yusufmafif <yusufmafif@gmail.com>
2024-05-31 20:17:50 +0700

    a

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 next();
 });
 
