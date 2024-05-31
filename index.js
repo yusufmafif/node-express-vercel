6c004359779ae8a34c2ecc1feadff95a362fb357
yusufmafif <yusufmafif@gmail.com>
2024-05-31 16:23:20 +0700

    fix cookie 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Middleware to set CORS headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Origin", process.env.DEV_ADDRESS);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
