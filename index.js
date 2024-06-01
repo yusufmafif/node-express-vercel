441c30bfb3e3cdfa8d7965e86b9bf1cbb80f0dbe
yusufmafif <yusufmafif@gmail.com>
2024-06-01 10:34:42 +0700

    test3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://cashier-pos.netlify.app', 
     credentials: true
 }));
 
