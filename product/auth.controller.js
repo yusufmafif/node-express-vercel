db3cbd4c80b28b3764e939dc577a586ffcd6faa6
yusufmafif <yusufmafif@gmail.com>
2024-06-01 14:17:28 +0700

    .

--- product/auth.controller.js
+++ product/auth.controller.js
@@ -6,20 +6,9 @@ const router = express.Router();
 
 const accessValidation = (req, res, next) => {
     const cookieHeader = (req.cookies)
-    console.log('cookieHeader:', cookieHeader);
     console.log(cookieHeader)
     console.log(cookieHeader.token)
-    
-    const parseCookies = (cookieHeader) => {
-        return cookieHeader.split(';').reduce((cookies, cookie) => {
-          const [name, value] = cookie.trim().split('=');
-          cookies[name] = value;
-          return cookies;
-        }, {});
-      };
-      const cookies = parseCookies(cookieHeader);
-
-    if (!cookies) {
+    if (!cookieHeader) {
         console.log("No cookie header present");
         return res.status(401).send({
             message: "Unauthorized 1",
@@ -27,7 +16,7 @@ const accessValidation = (req, res, next) => {
     }
 
     try {
-        const jwtDecode = jwt.verify(cookies, process.env.JWT_SECRET);
+        const jwtDecode = jwt.verify(cookieHeader.token, process.env.JWT_SECRET);
         req.userData = jwtDecode;
         next();
     } catch (error) {
@@ -39,6 +28,7 @@ const accessValidation = (req, res, next) => {
 }
 
 router.get("/", accessValidation, async (req, res) => {
+
     const userData = req.userData;
     console.log('Token validation successful for user:', userData);
     res.status(200).json({
