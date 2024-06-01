b5eaa069af27ac885458086e233ada9f0276ec01
yusufmafif <yusufmafif@gmail.com>
2024-06-01 18:34:57 +0700

    123321

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     next();
+// });
+app.use(cors({credentials: true, origin: 'https://cashier-pos.netlify.app'}))
 app.use(express.json());
 app.use(cookieParser()); 
 
